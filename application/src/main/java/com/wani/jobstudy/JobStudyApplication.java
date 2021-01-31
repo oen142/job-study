9fd49abfe0406ae9ef5159053889ba68ace275d3
oen142 <oen142@naver.com>
2021-01-31 18:27:08 +0900

    Initial commit

---
+++ application/src/main/java/com/wani/jobstudy/JobStudyApplication.java
@@ -0,0 +1,13 @@
+package com.wani.jobstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobStudyApplication.class, args);
+    }
+
+}
---
+++ application/src/test/java/com/wani/jobstudy/JobStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wani.jobstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
